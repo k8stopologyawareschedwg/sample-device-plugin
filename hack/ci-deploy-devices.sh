2c651eb405aeef640544b69e072b06b8c4adb031
Talor Itzhak <titzhak@redhat.com>
2022-09-19 16:01:21 +0300

    ci:e2e: simplifies e2e deployment
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- hack/ci-deploy-devices.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DIRNAME="$(dirname "$(readlink -f "$0")")"
-
-kubectl apply -f "$DIRNAME"/../config
-for ds in "$DIRNAME"/../manifests/deviceplugin*-ds.yaml; do
-  kubectl apply -f "$ds"
-done
